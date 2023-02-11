db02232aaa4baaf484c725ab9378a1e5618db252
Aaron C Wacker <30595158+AaronCWacker@users.noreply.github.com>
2023-02-11 00:53:34 -0600

    Create DDaac Eye imaging ai.py

---
+++ DDaac Eye imaging ai.py
@@ -0,0 +1,53 @@
+do you know how many plotly charts you can make, the variations?
+Plotly is a powerful library for creating interactive visualizations in Python. It provides a wide range of chart types that you can use to visualize data and communicate insights. Some of the chart types available in Plotly include:
+
+Scatter Plots
+Line Charts
+Bar Charts
+Pie Charts
+Histograms
+Box Plots
+Violin Plots
+Heat Maps
+Contour Plots
+3D Surface Plots
+Waterfall Charts
+Choropleth Maps
+Sankey Diagrams
+This is by no means an exhaustive list, as Plotly provides many other chart types, including specialized charts for financial, scientific, and geographic data. Additionally, within each chart type, there are many customization options available, allowing you to adjust the appearance, behavior, and data representation of the chart to meet your needs.
+
+
+
+
+Aaron Wacker
+how many did you say?
+I apologize for the confusion, there are more than 12 chart types available in Plotly, including:
+
+Scatter Plots
+Line Charts
+Bar Charts
+Pie Charts
+Histograms
+Box Plots
+Violin Plots
+Heat Maps
+Contour Plots
+3D Surface Plots
+Waterfall Charts
+Choropleth Maps
+Sankey Diagrams
+And many more specialized charts for financial, scientific, and geographic data. Additionally, within each chart type, there are many customization options available, allowing you to adjust the appearance, behavior, and data representation of the chart to meet your needs.
+
+
+
+
+Aaron Wacker
+no. repeat back to me, terse is good.  Cicero once said what?
+Cicero said, "Brevity is the soul of wit."
+
+
+
+
+Aaron Wacker
+witit yo!
+Yes, indeed! Brevity can make writing and communication more effective and impactful. By being concise and to the point, you can get your message across clearly and efficiently.
